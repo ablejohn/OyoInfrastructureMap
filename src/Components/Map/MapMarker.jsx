ec7fed4edfee1153631902983713f992e9067514
ABLE JOHN <johnabe410@gmail.com>
2024-09-16 16:28:33 +0100

    Latest Update

--- src/Components/Map/MapMarker.jsx
+++ src/Components/Map/MapMarker.jsx
@@ -1,25 +1,39 @@
 import React, { useRef } from "react";
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
 const MapMarker = ({ item, handleMarkerClick, map }) => {
   const popupRef = useRef();
 
   const handlePopupClose = () => {
-    // Check if map instance is available
     if (map) {
-      // Zoom out to a lower level (e.g., zoom level 8)
       map.setZoom(8);
     }
   };
+
+  // Create a custom icon with different colors based on the type
+  const customIcon = L.divIcon({
+    className: "custom-div-icon",
+    html: `<div style="background-color: ${
+      item.type === "hospital" ? "red" : "green"
+    }; 
+                       width: 30px; 
+                       height: 30px; 
+                       border-radius: 50%; 
+                       border: 2px solid white;">
+           </div>`,
+    iconSize: [15, 15],
+    iconAnchor: [7, 7],
+  });
 
   return (
     <Marker
       key={item.id}
       position={[item.lat, item.lng]}
+      icon={customIcon}
       eventHandlers={{
         click: () => {
           handleMarkerClick(item.lat, item.lng, 13);
-          // Open the popup and attach close event handler
           if (popupRef.current) {
             popupRef.current.openOn(map);
           }
