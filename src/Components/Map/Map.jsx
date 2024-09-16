a3bdd6b8b19f5b28a9c59613f39e8f60d285a830
ABLE JOHN <johnabe410@gmail.com>
2024-09-16 13:01:25 +0100

    Update

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import MapMarker from "./MapMarker"; 
+import MapMarker from "./MapMarker";
 
+// Update Leaflet icon options
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
@@ -21,20 +22,25 @@ function ChangeView({ center, zoom }) {
   return null;
 }
 
-const MapComponent = ({
-  mapCenter,
-  mapZoom,
-  filteredData,
-  setMapCenter,
-  setMapZoom,
-}) => {
+const MapComponent = ({ mapCenter, mapZoom, filteredData, setMapCenter, setMapZoom }) => {
+  const [mapInstance, setMapInstance] = useState(null);
+  const mapRef = useRef();
+
   const handleMarkerClick = (lat, lng, zoom) => {
     setMapCenter([lat, lng]);
     setMapZoom(zoom);
   };
 
   return (
-    <MapContainer center={mapCenter} zoom={mapZoom} style={{ height: "500px" }}>
+    <MapContainer
+      center={mapCenter}
+      zoom={mapZoom}
+      style={{ height: "500px" }}
+      whenCreated={(map) => {
+        setMapInstance(map);
+        mapRef.current = map;
+      }}
+    >
       <ChangeView center={mapCenter} zoom={mapZoom} />
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {filteredData.map((item) => (
@@ -42,6 +48,7 @@ const MapComponent = ({
           key={item.id}
           item={item}
           handleMarkerClick={handleMarkerClick}
+          map={mapInstance} // Pass map instance to MapMarker
         />
       ))}
     </MapContainer>
--- src/Components/Map/MapMarker.jsx
+++ src/Components/Map/MapMarker.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Marker, Popup } from "react-leaflet";
 
-const MapMarker = ({ item, handleMarkerClick }) => {
+const MapMarker = ({ item, handleMarkerClick, map }) => {
+  const popupRef = useRef();
+
+  const handlePopupClose = () => {
+    // Check if map instance is available
+    if (map) {
+      // Zoom out to a lower level (e.g., zoom level 8)
+      map.setZoom(8);
+    }
+  };
+
   return (
     <Marker
       key={item.id}
       position={[item.lat, item.lng]}
       eventHandlers={{
-        click: () => handleMarkerClick(item.lat, item.lng, 13),
+        click: () => {
+          handleMarkerClick(item.lat, item.lng, 13);
+          // Open the popup and attach close event handler
+          if (popupRef.current) {
+            popupRef.current.openOn(map);
+          }
+        },
       }}
     >
-      <Popup>
+      <Popup
+        ref={popupRef}
+        onClose={handlePopupClose}
+      >
         <strong>{item.name}</strong>
         <br />
         Type: {item.type}
